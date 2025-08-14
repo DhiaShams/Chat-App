bc7301e57b03a123df51a23b68b115695c731387
DhiaShams <dhiadhilu123@gmail.com>
2025-08-14 11:00:28 +0530

    Changed logo_icon

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -17,7 +17,7 @@ const ChatContainer = ({selectedUser, setSelectedUser}) => {
   ): (
     
     <div className='flex flex-col items-center justify-center gap-2 text-gray-500 bg-white/10 max-md:hidden'>
-      <img src={assets.logo} alt="" className='max-w-16'/>
+      <img src={assets.logo_icon} alt="" className='max-w-16'/>
       <p className='text-lg font-medium text-white'>Connect, Chat, and Elevate Conversations</p>
     </div>
   )
